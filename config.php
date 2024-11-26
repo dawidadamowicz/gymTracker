62e77ef5fff1e2e31d797eeb6d677a2c1b4d2d4b
Dawid Adamowicz <122816840+dawidadamowicz@users.noreply.github.com>
2024-11-26 23:43:07 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';  // Ensure the autoload file is included
+require_once __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
@@ -12,17 +12,14 @@ class Config {
     private $pdo;
 
     public function __construct() {
-        // Load the .env file and set up the environment variables
         $dotenv = Dotenv::createImmutable(__DIR__);
         $dotenv->load();
 
-        // Assign values from environment variables to the class properties
         $this->host = $_ENV['DB_HOST'];
         $this->dbname = $_ENV['DB_NAME'];
         $this->username = $_ENV['DB_USER'];
         $this->password = $_ENV['DB_PASSWORD'];
 
-        // Connect to the database
         $this->connect();
     }
 
